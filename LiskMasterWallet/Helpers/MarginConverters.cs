469f29eb12c0ad0bed39dee18dfdf5421c8b9ec4
ByronAP <byronp@cryptomic.com>
2016-06-22 21:46:58 -0400

    voting

---
+++ LiskMasterWallet/Helpers/MarginConverters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LiskMasterWallet.Helpers
+{
+    public class LeftMarginConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return new Thickness(System.Convert.ToDouble(value), 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class RightMarginConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return new Thickness( 0, 0, System.Convert.ToDouble(value), 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
